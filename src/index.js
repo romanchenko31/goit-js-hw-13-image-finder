d772d9c9e951573f0b3f624f77bd1024dd76824c
romanchenko31 <romanchenko.zhen@gmail.com>
2022-09-15 20:05:50 +0300

    add code js

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import card from './template/card.hbs';
+import fetchUsers from './js/apiService.js';
+import { scrollIntoView } from 'scroll-js';
+
+const debounce = require('lodash.debounce');
+const formInput = document.querySelector('form');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+let firstElementAfterClickLoadMore = 12;
+
+formInput.addEventListener('input', debounce(targetValueImg, 1000));
+
+const promiseListImg = async (value, pageNumber) => {
+    const promisListImg = await fetchUsers(value, pageNumber);
+    const arrImg = await promisListImg.hits;  
+    addMarcup(pageNumber, arrImg);
+}
+
+function targetValueImg(e, pageNumber) {
+    value = e.target.value;
+    pageNumber = 1;
+    promiseListImg(value, pageNumber);
+    buttonLoadMore(value ,pageNumber);
+}
+
+function buttonLoadMore(value ,pageNumber) {
+     button.addEventListener('click', () => {
+        pageNumber += 1;
+         promiseListImg(value, pageNumber);
+     });
+    
+}
+    
+function scrollElement() {
+    const myElement = document.body.getElementsByTagName('li');            
+    scrollIntoView(myElement[firstElementAfterClickLoadMore], document.body, { behavior: 'smooth' });
+    firstElementAfterClickLoadMore += 12;
+} 
+ 
+function addMarcup(pageNumber, arrImg) {
+      const marcup = card(arrImg);
+    if (pageNumber === 1) {
+        ul.innerHTML = marcup;
+    } else {
+        ul.insertAdjacentHTML('beforeend', marcup);
+        scrollElement();
+    }
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,13 @@
+const KEY_API = '29920184-cb57a37281186e19b397ef599';
+const URL = 'pixabay.com/api';
+
+export default fetchUsers = async (values, pageNumber) => {
+    try {
+        const response = await fetch(`https://${URL}/?image_type=photo&q=${values}&orientation=horizontal&page=${pageNumber}&per_page=12&key=${KEY_API}`);
+        const users = await response.json();
+        return users;
+    }catch(error) {
+        console.log('hello',error);
+    }
+    
+}
