fc9d570802eae9a6d6627fb89c19582dcba274a8
romanchenko31 <romanchenko.zhen@gmail.com>
2022-09-16 15:28:45 +0300

    add edit fetchUser

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,8 +1,8 @@
-
-
-export default fetchUsers = async (values, pageNumber) => {
     const KEY_API = '29920184-cb57a37281186e19b397ef599';
     const URL = 'pixabay.com/api';
+
+
+ const fetchUsers = async (values, pageNumber) => {
     try {
         const response = await fetch(`https://${URL}/?image_type=photo&q=${values}&orientation=horizontal&page=${pageNumber}&per_page=12&key=${KEY_API}`);
         const users = await response.json();
@@ -12,3 +12,4 @@ export default fetchUsers = async (values, pageNumber) => {
     }
     
 }
+export default fetchUsers;
